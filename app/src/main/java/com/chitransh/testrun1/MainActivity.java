113c9c6206ae391f58e75d19084c89fe58b92680
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-17 18:18:23 +0530

    imp

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -3,6 +3,7 @@ package com.chitransh.testrun1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Log.e("Event: ", "create");
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    Log.e("Event: ", "start");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    Log.e("Event: ", "pause");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    Log.e("Event: ", "resume");
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+
+    Log.e("Event: ", "stop");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    Log.e("Event: ", "destroy");
   }
 }
