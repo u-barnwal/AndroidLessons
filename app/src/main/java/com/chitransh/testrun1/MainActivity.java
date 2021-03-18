425e0f8aef97e40044fdfce5955efa11ab87079a
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-18 18:38:53 +0530

    imp

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.chitransh.testrun1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button button = findViewById(R.id.button);
+
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Log.e("==: ", "Button clicked!");
+      }
+    });
+
+    TextView textView = findViewById(R.id.textView);
+
+    textView.setOnClickListener((v) -> {
+      Log.e("==: ", "Text clicked!");
+    });
+
+//    textView.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//        Log.e("==: ", "Text clicked!");
+//      }
+//    });
+
   }
 }
