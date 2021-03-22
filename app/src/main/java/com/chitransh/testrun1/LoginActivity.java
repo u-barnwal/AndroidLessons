e08b340adc988408df403483854e23dc893a9e1a
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-22 18:08:23 +0530

    add LoginActivity

---
+++ app/src/main/java/com/chitransh/testrun1/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.chitransh.testrun1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+  private final String registeredEmail = "demo@test.com";
+  private final String registeredPassword = "123";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+
+    EditText etEmail = findViewById(R.id.etEmail);
+    EditText etPassword = findViewById(R.id.etPassword);
+
+    Button bLogin = findViewById(R.id.bLogin);
+
+    TextView tvForgotPassword = findViewById(R.id.tvForgotPassword);
+
+    bLogin.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (
+          etEmail.getText().toString().equals(registeredEmail) &&
+            etPassword.getText().toString().equals(registeredPassword)
+        ) {
+          Log.e("----", "Login Successful!");
+        } else {
+          Log.e("----", "Email or Password incorrect!");
+        }
+      }
+    });
+  }
+}
--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,44 +1,23 @@
 package com.chitransh.testrun1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-  private final String registeredEmail = "demo@test.com";
-  private final String registeredPassword = "123";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    EditText etEmail = findViewById(R.id.etEmail);
-    EditText etPassword = findViewById(R.id.etPassword);
-
     Button bLogin = findViewById(R.id.bLogin);
 
-    TextView tvForgotPassword = findViewById(R.id.tvForgotPassword);
-
-    bLogin.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (
-          etEmail.getText().toString().equals(registeredEmail) &&
-            etPassword.getText().toString().equals(registeredPassword)
-        ) {
-          Log.e("----", "Login Successful!");
-        } else {
-          Log.e("----", "Email or Password incorrect!");
-        }
-      }
+    bLogin.setOnClickListener(v -> {
+      Intent intent = new Intent(this, LoginActivity.class);
+      startActivity(intent);
     });
-
   }
 }
