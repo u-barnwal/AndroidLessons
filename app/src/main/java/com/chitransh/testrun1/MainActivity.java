c50997f308999a430023e161147fa68f46baec6f
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-23 18:29:11 +0530

    add webvite

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -3,6 +3,7 @@ package com.chitransh.testrun1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    WebView wvMain = findViewById(R.id.wvMain);
+
+    wvMain.loadUrl("https://wikipedia.org");
   }
 }
