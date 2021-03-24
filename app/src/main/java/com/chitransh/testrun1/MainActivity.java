e9f75728861b0f1182cd0ce4a8f640aaf560ffa8
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-24 18:33:42 +0530

    ex: Spinner

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -2,10 +2,9 @@ package com.chitransh.testrun1;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    String[] platforms = getResources().getStringArray(R.array.platform);;
 
 //    ProgressBar pbMain = findViewById(R.id.pbMain);
 //    Button bShowPB = findViewById(R.id.bShowPB);
@@ -29,30 +30,44 @@ public class MainActivity extends AppCompatActivity {
 ////      pbMain.setVisibility(View.GONE);
 //    });
 
-    Switch sToggle = findViewById(R.id.sToggle);
-    TextView tvContent = findViewById(R.id.tvContent);
+//    Switch sToggle = findViewById(R.id.sToggle);
+//    TextView tvContent = findViewById(R.id.tvContent);
+//
+//    CheckBox cbMain = findViewById(R.id.cbMain);
+//
+//    sToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//      @Override
+//      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//        cbMain.setChecked(isChecked);
+//        if (isChecked)
+//          tvContent.setVisibility(View.VISIBLE);
+//        else
+//          tvContent.setVisibility(View.GONE);
+//      }
+//    });
+//
+//    cbMain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//      @Override
+//      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//        sToggle.setChecked(isChecked);
+//        if (isChecked)
+//          tvContent.setVisibility(View.VISIBLE);
+//        else
+//          tvContent.setVisibility(View.GONE);
+//      }
+//    });
 
-    CheckBox cbMain = findViewById(R.id.cbMain);
+    Spinner sPlatform = findViewById(R.id.sPlatform);
 
-    sToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    sPlatform.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
       @Override
-      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        cbMain.setChecked(isChecked);
-        if (isChecked)
-          tvContent.setVisibility(View.VISIBLE);
-        else
-          tvContent.setVisibility(View.GONE);
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), "Item selected " + platforms[position], Toast.LENGTH_SHORT).show();
       }
-    });
 
-    cbMain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       @Override
-      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        sToggle.setChecked(isChecked);
-        if (isChecked)
-          tvContent.setVisibility(View.VISIBLE);
-        else
-          tvContent.setVisibility(View.GONE);
+      public void onNothingSelected(AdapterView<?> parent) {
+
       }
     });
 
