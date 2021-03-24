bf65f3d748b5fd41edc5674a9e217c79bada7b05
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-24 18:19:10 +0530

    ex: progressbar, switch

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.chitransh.testrun1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+//    ProgressBar pbMain = findViewById(R.id.pbMain);
+//    Button bShowPB = findViewById(R.id.bShowPB);
+//    Button bHidePB = findViewById(R.id.bHidePB);
+//
+//    bShowPB.setOnClickListener(v -> {
+//      pbMain.setVisibility(View.VISIBLE);
+//    });
+//
+//    bHidePB.setOnClickListener(v -> {
+//      pbMain.setVisibility(View.INVISIBLE);
+////      pbMain.setVisibility(View.GONE);
+//    });
+
+    Switch sToggle = findViewById(R.id.sToggle);
+    TextView tvContent = findViewById(R.id.tvContent);
+
+    sToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked)
+          tvContent.setVisibility(View.VISIBLE);
+        else
+          tvContent.setVisibility(View.GONE);
+      }
+    });
+
   }
 }
