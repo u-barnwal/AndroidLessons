dcfa9b290766054f96e5e600ba3149586bb1adcb
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-25 18:13:14 +0530

    imp

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,28 +1,36 @@
 package com.chitransh.testrun1;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+  private Activity activity;
+  private SharedPreferences sharedPreferences;
+  private EditText etName;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    SharedPreferences sharedPreferences = getSharedPreferences("SP_TESTRUN1", MODE_PRIVATE);
+    activity = this;
 
-    EditText etName = findViewById(R.id.etName);
+    sharedPreferences = getSharedPreferences("SP_TESTRUN1", MODE_PRIVATE);
+
+    etName = findViewById(R.id.etName);
     Button bSave = findViewById(R.id.bSave);
 
 //    if(sharedPreferences.contains("username"))
-      etName.setText(sharedPreferences.getString("username", ""));
+    etName.setText(sharedPreferences.getString("username", ""));
 
     bSave.setOnClickListener(v -> {
       if (etName.getText() == null || etName.getText().toString().isEmpty()) {
@@ -30,11 +38,31 @@ public class MainActivity extends AppCompatActivity {
         return;
       }
 
-      SharedPreferences.Editor editor = sharedPreferences.edit();
-      editor.putString("username", etName.getText().toString());
-      editor.apply();
+      AlertDialog.Builder builder = new AlertDialog.Builder(activity);
 
-      Toast.makeText(getApplicationContext(), "Names save!", Toast.LENGTH_SHORT).show();
+      builder.setTitle("Are you sure?");
+      builder.setMessage("This name will be saved to the application. Continue?");
+
+      builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+          saveName();
+        }
+      });
+
+      builder.setNegativeButton("No", (dialog, which) -> {
+        etName.setText("");
+      });
+
+      builder.show();
     });
+  }
+
+  private void saveName() {
+    SharedPreferences.Editor editor = sharedPreferences.edit();
+    editor.putString("username", etName.getText().toString());
+    editor.apply();
+
+    Toast.makeText(getApplicationContext(), "Names save!", Toast.LENGTH_SHORT).show();
   }
 }
