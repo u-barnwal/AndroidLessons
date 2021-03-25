41b7eddbe62509b293b30e7b93aca74d2c7cb88c
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-25 17:59:29 +0530

    imp

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,8 +1,13 @@
 package com.chitransh.testrun1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    SharedPreferences sharedPreferences = getSharedPreferences("SP_TESTRUN1", MODE_PRIVATE);
+
+    EditText etName = findViewById(R.id.etName);
+    Button bSave = findViewById(R.id.bSave);
+
+//    if(sharedPreferences.contains("username"))
+      etName.setText(sharedPreferences.getString("username", ""));
+
+    bSave.setOnClickListener(v -> {
+      if (etName.getText() == null || etName.getText().toString().isEmpty()) {
+        Toast.makeText(getApplicationContext(), "You must enter your name!", Toast.LENGTH_SHORT).show();
+        return;
+      }
+
+      SharedPreferences.Editor editor = sharedPreferences.edit();
+      editor.putString("username", etName.getText().toString());
+      editor.apply();
+    });
   }
 }
