6061ec66e853077aa843b6e8163dfff9d41e2d0e
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-26 17:57:45 +0530

    imp ArrayAdapter

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,24 +1,41 @@
 package com.chitransh.testrun1;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+  private ListView lvMain;
+  private ArrayList list;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    lvMain = findViewById(R.id.lvMain);
+
+    list = new ArrayList();
+
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    list.add("D");
+    list.add("E");
+    list.add("F");
+    list.add("G");
+    list.add("G");
+    list.add("H");
+    list.add("I");
+
+    ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+
+    lvMain.setAdapter(adapter);
   }
 
 }
