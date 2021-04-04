4061de7332febf55c958f9daa961749cd8a78726
Utsav Barnwal <u.barnwal36@gmail.com>
2021-04-05 04:54:55 +0530

    imp

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,8 +1,19 @@
 package com.chitransh.testrun1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,41 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    EditText etTitle = findViewById(R.id.etTitle);
+    EditText etBody = findViewById(R.id.etBody);
+    Button bPush = findViewById(R.id.bPush);
+
+    bPush.setOnClickListener(v -> {
+      String title = etTitle.getText().toString();
+      String body = etBody.getText().toString();
+
+      Intent intent = new Intent(this, MainActivity.class);
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+      PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+      String channelId = "DEFAULT_NOTIFICATION_CHANNEL";
+//      Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+      NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setContentTitle(title)
+        .setContentText(body)
+        .setAutoCancel(true)
+//        .setOngoing(true)
+//        .setStyle(new NotificationCompat.BigTextStyle().bigText(body))
+//        .setSound(defaultSoundUri)
+        .setContentIntent(pendingIntent);
+
+      NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        NotificationChannel channel = new NotificationChannel(channelId, "Default Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+        notificationManager.createNotificationChannel(channel);
+      }
+
+      notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+    });
   }
 }
