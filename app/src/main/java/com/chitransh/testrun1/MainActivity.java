e1005f2f66aedb70fa806a871e3712754b6b9e5d
Utsav Barnwal <u.barnwal36@gmail.com>
2021-04-05 05:31:03 +0530

    basic imp

--- app/src/main/java/com/chitransh/testrun1/MainActivity.java
+++ app/src/main/java/com/chitransh/testrun1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.chitransh.testrun1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    TextView tvContacts = findViewById(R.id.tvContacts);
+
+    ContentResolver contentResolver = getContentResolver();
+
+    Cursor cursor = contentResolver.query(
+      ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+      null, // which all columns you want to select
+      null, // where statement
+      null, // where statement values
+      null // sorting order (pass column names)
+    );
+
+    String contacts = "";
+
+    if (cursor != null && cursor.getCount() > 0) {
+      while (cursor.moveToNext()) {
+        contacts +=
+          cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+            + " " +
+            cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+            + "\n";
+      }
+    }
+
+    tvContacts.setText(contacts);
+
+    cursor.close();
   }
 }
